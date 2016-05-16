20e2a9f18ce7cb70af49c2df42adc8807a29c3fd
Isheng <isheng5@users.noreply.github.com>
2016-05-16 17:19:43 +0800

    BASE

---
+++ 基础/if-elase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+a = 100
+if a >= 0:
+    print a
+    a = a-1
+else:
+    print 0
+
+print 3 > 2
+print True and True
+print not True
--- 基础/输入输出.py
+++ 基础/输入输出.py
@@ -1,5 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# 输出部分
 
 print 'hello,world'
-print 'The quick brown fox','jumps over','the lazy dog'
+print 'The quick brown fox', 'jumps over', 'the lazy dog'
 print 100 + 200
-print '100 + 200 =',100 + 200
+print '100 + 200 =', 100 + 200
+
+# 输入部分
+
+name = raw_input('请输入你的名字：'.decode('utf-8').encode('utf-8'))
+print 'hello',name
