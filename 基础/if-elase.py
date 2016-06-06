000ad7fafe08d6894e9c9ffd0c976ddba2c18bb9
Isheng <isheng5@users.noreply.github.com>
2016-06-06 13:45:38 +0800

    BASE

--- 基础/if-elase.py
+++ 基础/if-elase.py
@@ -11,3 +11,12 @@ else:
 print 3 > 2
 print True and True
 print not True
+classmates = ['Michael', 'Bob', 'Tracy']
+print classmates[0]
+print classmates[-1]
+addclass = raw_input()
+classmates.append(addclass)
+print classmates[-1]
+classmates.pop(-1)
+print classmates[-1]
+print len(classmates)
